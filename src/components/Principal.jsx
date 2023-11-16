c57db01029ad78f7c96e0e09eebec7e3ef9a1414
Uriel <012011355@upq.edu.mx>
2023-11-15 23:51:21 -0600

    agregando outlet

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 export  const Principal = ()=>{
     return <>
@@ -19,6 +19,6 @@ export  const Principal = ()=>{
             </div>
             
         </nav>
-    
+        <Outlet></Outlet>
     </>
 }
