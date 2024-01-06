7208e65aab253bbf59c9d3bef44d1308e6e10645
uri1490 <012011355@upq.edu.mx>
2024-01-06 17:17:21 -0600

    se agrega tailwind

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export  const Principal = ()=>{
-    return <>
-    
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container">
-                <ul>
-                    <li>
+export const Principal = () => {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <ul>
+            test
+            {/* <li>
                         <Link to="/">Home</Link>
                     </li>
                     <li>
@@ -14,11 +15,10 @@ export  const Principal = ()=>{
                     </li>
                     <li>
                         <Link to="/Projects">Projects</Link>
-                    </li>        
-                </ul>
-            </div>
-            
-        </nav>
-    
+                    </li>         */}
+          </ul>
+        </div>
+      </nav>
     </>
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Layout } from './components/Layout'
-import { RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Layout } from "./components/Layout";
+import { RouterProvider } from "react-router-dom";
 //import App from './App.jsx'
-//import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <RouterProvider router={Layout}></RouterProvider>
-  </React.StrictMode>,
-)
+    <RouterProvider router={Layout}></RouterProvider>
+  </React.StrictMode>
+);
