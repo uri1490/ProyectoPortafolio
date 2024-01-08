c2f2ac1606d3993bd8930b726be3f787701c57b8
uri1490 <012011355@upq.edu.mx>
2024-01-08 17:34:09 -0600

    agregan imagenes

--- src/components/Credencial.jsx
+++ src/components/Credencial.jsx
@@ -7,12 +7,24 @@ export const Credencial = () => {
         id="tarjeta"
         className="bg-blue-300 flex justify-center flex-col-reverse md:flex-row h-[50%]"
       >
-        <div id="descripcion" className="bg-red-600">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-          nobis.
+        <div id="descripcion" className="bg-red-600 flex flex-col">
+          <span className="flex justify-center">Uriel RV</span>
+          <span className="flex justify-center">Desarrollador Web</span>
+          <span>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
+            nobis.
+          </span>
+          <div></div>
+          <div className="flex flex-row justify-center space-x-6">
+            <img src="./src/assets/code.png" alt="" className="w-10 h-10" />
+            <img src="./src/assets/linkedin.png" alt="" className="w-10 h-10" />
+          </div>
         </div>
-        <div id="foto" className="bg-green-200 flex items-center">
-          foto
+        <div
+          id="foto"
+          className="bg-green-200 flex justify-center md:items-center"
+        >
+          <div className="bg-black w-14 h-14 rounded-full"></div>
         </div>
       </div>
     </>
--- src/components/Descripcion.jsx
+++ src/components/Descripcion.jsx
@@ -6,6 +6,12 @@ export const Descripcion = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, maxime.
       Accusamus aliquam voluptas ipsa hic quod, eos modi quaerat beatae ratione
       commodi ducimus obcaecati! Ad aperiam fugit perferendis veniam rerum.
+      <div className="flex w-full justify-center">
+        <button className="flex items-center py-1 px-4 bg-blue-500 border border-blue-700 rounded space-x-3">
+          <img src="./src/assets/CV1.ico" alt="" />
+          <span>CV</span>
+        </button>
+      </div>
     </div>
   );
 };
