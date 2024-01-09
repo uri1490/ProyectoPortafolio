3f643339a7d9ea67a229960e5b7577fc5bca545f
uri1490 <012011355@upq.edu.mx>
2024-01-08 18:53:30 -0600

    pruebas pdf

--- src/components/Descripcion.jsx
+++ src/components/Descripcion.jsx
@@ -7,10 +7,16 @@ export const Descripcion = () => {
       Accusamus aliquam voluptas ipsa hic quod, eos modi quaerat beatae ratione
       commodi ducimus obcaecati! Ad aperiam fugit perferendis veniam rerum.
       <div className="flex w-full justify-center">
-        <button className="flex items-center py-1 px-4 bg-blue-500 border border-blue-700 rounded space-x-3">
-          <img src="./src/assets/CV1.ico" alt="" />
-          <span>CV</span>
-        </button>
+        <a
+          href="../../public/certificado.pdf"
+          //   download={"../..public/certificado.pdf"}
+          target="_blank"
+        >
+          <button className="flex items-center py-1 px-4 bg-blue-500 border border-blue-700 rounded space-x-3">
+            <img src="./src/assets/CV1.ico" alt="" />
+            <span>CV</span>
+          </button>
+        </a>
       </div>
     </div>
   );
