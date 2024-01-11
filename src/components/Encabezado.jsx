ee254680edbdce51abfa05d12d756f4f3cef2442
uri1490 <012011355@upq.edu.mx>
2024-01-10 23:58:52 -0600

    corregimos responsive navbar

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 
+import { useState } from "react";
+import { ImMenu3 } from "react-icons/im";
 export const Encabezado = () => {
+  const [openNav, setOpenNav] = useState(false);
+  const handleClick = () => setOpenNav(!openNav);
+  const closeMobileMenu = () => setOpenNav(false);
+  console.log(openNav);
   return (
     <>
-      <nav className="navbar bg-red-950 flex bg-dark w-full justify-between text-red-500 h-[5vh] sticky top-0">
-        <div>logo</div>
-        <div className="space-x-2">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#projects">Proyectos</a>
+      <nav className="navbar bg-red-950 flex bg-dark w-full justify-between text-red-500 h-[5vh] md:h-[5vh] sticky top-0">
+        <div className="z-50">logo</div>
+        <div className="z-40 md:hidden">
+          <button onClick={handleClick}>
+            <ImMenu3 className="w-7 h-7"></ImMenu3>
+          </button>
+        </div>
+        <div
+          className={`absolute  md:static md:flex  bg-pink-400 ${
+            openNav ? "flex" : "hidden"
+          } justify-center w-full md:w-1/2 h-[100vh] md:h-full items-center z-10`}
+        >
+          <ul className="flex w-full bg-slate-300 justify-center space-y-10 md:space-y-0 flex-col md:flex-row md:justify-between">
+            <li className="flex justify-center">
+              <a href="#home">Home</a>
+            </li>
+            <li className="flex justify-center">
+              <a href="#about">About</a>
+            </li>
+            <li className="flex justify-center">
+              <a href="#projects">Proyectos</a>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
