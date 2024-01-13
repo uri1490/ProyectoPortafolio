34b6a4d9e2ad9296aa241f24a21ddd84b13f7ac8
uri1490 <012011355@upq.edu.mx>
2024-01-13 00:02:49 -0600

    ajustamos tarjeta

--- src/components/Credencial.jsx
+++ src/components/Credencial.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-
+import { FaUserAstronaut } from "react-icons/fa6";
 export const Credencial = () => {
   return (
     <>
       <div
         id="tarjeta"
-        className="bg-blue-300 flex justify-center flex-col-reverse md:flex-row h-[50%]"
+        className="bg-blue-300 flex flex-col-reverse w-1/2 rounded-2xl md:flex-row h-[50%] overflow-hidden"
       >
-        <div id="descripcion" className="bg-red-600 flex flex-col">
-          <span className="flex justify-center">Uriel RV</span>
-          <span className="flex justify-center">Desarrollador Web</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-            nobis.
-          </span>
-          <div></div>
+        <div
+          id="descripcion"
+          className="bg-red-600 flex flex-col justify-around md:w-9/12"
+        >
+          <div>
+            <span className="flex justify-center">Hola, Soy</span>
+            <span className="flex justify-center text-[3rem]">Uriel RV</span>
+            <span className="flex justify-center">Soy Desarrollador Web</span>
+          </div>
+
           <div className="flex flex-row justify-center space-x-6">
             <img src="./code.png" alt="code" className="w-10 h-10" />
             <img src="./linkedin.png" alt="link" className="w-10 h-10" />
@@ -22,9 +24,11 @@ export const Credencial = () => {
         </div>
         <div
           id="foto"
-          className="bg-green-200 flex justify-center md:items-center"
+          className="bg-green-200 flex justify-center items-center w-full md:w-1/4 h-full"
         >
-          <div className="bg-black w-14 h-14 rounded-full"></div>
+          <div className=" w-20 h-20 rounded-full border-solid border-2 border-indigo-600 overflow-hidden">
+            <FaUserAstronaut className="w-full h-full" />
+          </div>
         </div>
       </div>
     </>
